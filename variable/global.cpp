28bed997e7b00a8115e63ae3c2c89625c7082b0c
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2023-07-25 17:47:34 +0530

    Add files via upload

---
+++ variable/global.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//gobal variable
+int g;
+int main()
+{
+    
+    int a,b;
+    a=10;
+    b=4;
+    g=a+b;
+    cout << g;
+    return 0;
+
+}
---
+++ variable/local.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //local variabel
+    int a, b;
+    int c;
+
+    a=4;
+    b=14;
+    c=a+b;
+    cout << c;
+    return 0;
+
+}
